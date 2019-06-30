6f515041202ef058cc59f7ef3e11e00ddc4be0bd
vmiis <zzm.vmiis@gmail.com>
2019-06-30 18:39:32 +1000

    update

--- sites/request-approval/module-list.js
+++ sites/request-approval/module-list.js
@@ -2,14 +2,18 @@
     //-------------------------------------------------------------------------------------
     var modules={
         "dashboard":  	    {url:"$H/m/dashboard.html",router:1},
-        "request-data":  	{url:"$H/m/request-data.html",Table:"request-demo-001",Table2:"approval-demo-001",form_module:"request-form",router:1},
-        "request-form":  	{url:"$H/m/request-form.html",Table:"request-demo-001",email_to_manager:"email-to-manager"},
+        "request-all-data": {url:"$H/m/request-data.html",Table:"request-demo-001",Table2:"approval-demo-001",form_module:"request-form",router:1,email_to_manager:"email-to-manager",all:1,title:'All Requests'},
+        "request-data":  	{url:"$H/m/request-data.html",Table:"request-demo-001",Table2:"approval-demo-001",form_module:"request-form",router:1,email_to_manager:"email-to-manager",title:'My Requests'},
+        "request-form":  	{url:"$H/m/request-form.html",Table:"request-demo-001",email_to_manager:"email-to-manager",manager_list_table:"email-list-demo-001"},
 
-        "approval-data":  	{url:"$H/m/approval-data.html",Table:"request-demo-001",Table2:"approval-demo-001",form_module:"approval-form",router:1},
-        "approval-form":  	{url:"$H/m/approval-form.html",Table:"approval-demo-001"},
+        "approval-data":  	{url:"$H/m/approval-data.html",Table:"request-demo-001",Table2:"approval-demo-001",form_module:"approval-form",router:1,title:'My Approvals'},
+        "approval-form":  	{url:"$H/m/approval-form.html",Table:"approval-demo-001",manager_list_table:"email-list-demo-001"},
         
         "email-to-manager": {url:"$H/m/email-to-manager.html",router:1},
         
+        "manager-list-data":  	{url:"$H/m/setup-email-list-data.html",Table:"email-list-demo-001",form_module:"manager-list-form",router:1,title:'Manager List'},
+        "manager-list-form":  	{url:"$H/m/setup-email-list-form.html",Table:"email-list-demo-001",title:'Manager List'},
+
 
         "about-vmiis":  {url:"$H/m/about-vmiis.html",router:1},
         "module-editor":{url:"https://vm.vmiis.com/component/m/module-editor.html",router:1,sys:1},
