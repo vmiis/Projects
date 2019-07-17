38d7ce9d686037e5b6d0fa002160c5e73c551302
vmiis <zzm.vmiis@gmail.com>
2019-07-17 12:37:08 +1000

    update

---
+++ sites/comeback/module-list.js
@@ -0,0 +1,16 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var modules={
+        "blog":                         {url:"$H/m/blog.html",router:1},
+        "send-message-data":            {url:"$H/m/send-message-data.html",Table:"send-message-demo190715", form_module:"send-message-form", router:1},
+        "send-message-form":            {url:"$H/m/send-message-form.html",Table:"send-message-demo190715" },
+        //-------------------------------------------------------------------------------------
+        "module-editor":                {url:"https://vm.vmiis.com/component/m/module-editor.html",router:1,sys:1},
+        //-------------------------------------------------------------------------------------
+    }
+    for(p in modules){
+        $vm.module_list[p]=modules[p]; 
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    //-------------------------------------------------------------------------------------
+})();
