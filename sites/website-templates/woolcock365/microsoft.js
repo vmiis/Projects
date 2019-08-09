33965ca2616b53a7472e2c5833f4d7a8467fe4a9
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-08-10 09:14:14 +1000

    update

--- sites/website-templates/woolcock365/microsoft.js
+++ sites/website-templates/woolcock365/microsoft.js
@@ -1,18 +1,18 @@
 $vm.m365_init=function(){
-    var hosting_path0=window.location.href.split('#')[0];
-    var hosting_path=hosting_path0.substring(0, hosting_path0.split('?')[0].lastIndexOf('/'));
+    var h=window.location.protocol+"//"+window.location.hostname;
+    var p=window.location.port;
+    if(p!="") h=h+":"+p;
+    
+    //var hosting_path0=window.location.href.split('#')[0];
+    //var hosting_path=hosting_path0.substring(0, hosting_path0.split('?')[0].lastIndexOf('/'));
 
-    $vm.m365_sharepoint_uri="https://woolcockmed.sharepoint.com";
-    $vm.m365_scope_sharepoint={
-        scopes: ["https://woolcockmed.sharepoint.com/.default"]
-    };
     $vm.m365_scope={
-        scopes: ["user.read","User.Read.All","Files.Read.All","Sites.ReadWrite.All"/*",group.read.all"*/]
+        scopes: ["user.read","AllSites.Read"] /*,"User.Read.All","Files.Read.All","Sites.ReadWrite.All"*/
     };
     $vm.msalConfig={
         auth: {
             clientId: '3bcb40c5-fec0-4b3b-ba67-f4d46d577f97', 
-            redirectUri:hosting_path+"/microsoft-authentication.html"
+            redirectUri:h+"/microsoft-authentication-w.html",
         },
         cache: {
             cacheLocation: "localStorage",
@@ -21,24 +21,25 @@ $vm.m365_init=function(){
     };
     $vm.m365_msal=new Msal.UserAgentApplication($vm.msalConfig);
     $vm.m365_signin=function (){
-        $vm.m365_msal.loginPopup($vm.m365_scope_sharepoint).then(function (loginResponse) {               
-            return $vm.m365_msal.acquireTokenSilent($vm.m365_scope_sharepoint);
+        $vm.m365_msal.loginPopup($vm.m365_scope).then(function (loginResponse) {               
+            return $vm.m365_msal.acquireTokenSilent($vm.m365_scope);
         }).then(function (accessTokenResponse) {
             $vm.m365_init();
         }).catch(function (error) {  
             console.log(error);
+            alert(error)
         });
     }
     $vm.m365_signout=function() {
         $vm.m365_msal.logout();
     }
-    $vm.m365_graph=function(url,callback){
-        $vm.m365_msal.acquireTokenSilent($vm.m365_scope).then(function (tokenResponse) {
+    $vm.m365_graph=function(url,scope,callback){
+        $vm.m365_msal.acquireTokenSilent(scope).then(function (tokenResponse) {
             var xmlHttp = new XMLHttpRequest();
             var mt1=new Date().getTime();
             xmlHttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200){
-                    $("#_sys_dev_info_elapsed").html( (this.response.length/1000).toFixed(1)+"kb/"+(new Date().getTime()-mt1).toString()+"ms");
+                    //$("#_sys_dev_info_elapsed").html( (this.response.length/1000).toFixed(1)+"kb/"+(new Date().getTime()-mt1).toString()+"ms");
                     var data=JSON.parse(this.responseText);
                     callback(data);
                 }
@@ -53,6 +54,7 @@ $vm.m365_init=function(){
             }
             xmlHttp.open("GET", url, true); // true for asynchronous
             xmlHttp.setRequestHeader('Authorization', 'Bearer ' + tokenResponse.accessToken);
+            xmlHttp.setRequestHeader("Accept", "application/json");
             xmlHttp.send();
         }).catch(function (error){
             console.log(error);
@@ -62,7 +64,7 @@ $vm.m365_init=function(){
     //------------------------------------
     if($vm.m365_msal.getAccount()!=undefined){
         console.log($vm.m365_msal.getAccount().userName);
-        $vm.m365_msal.acquireTokenSilent($vm.m365_scope_sharepoint).then(function (tokenResponse) {
+        $vm.m365_msal.acquireTokenSilent({scopes: ["user.read"]}).then(function (tokenResponse){
             console.log("acquireTokenSilent succesfull.");
             $vm.user_name_3rd=$vm.m365_msal.getAccount().name;
             $vm.issuer_3rd="microsoft";
@@ -82,4 +84,5 @@ $vm.m365_init=function(){
     //------------------------------------
 }
 $vm.m365_init();
-setInterval(function(){ console.log("microsoft refresh"); $vm.m365_init(); }, 1800000);
+//setInterval(function(){ console.log("microsoft refresh"); $vm.m365_init(); }, 1800000);
+
