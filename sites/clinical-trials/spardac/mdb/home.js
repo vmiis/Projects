ab95a38d0c8c03f25f06b091ecd54dc19c42d5bf
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-08-19 16:15:27 +1000

    update

--- sites/clinical-trials/spardac/mdb/home.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    var modules={
-        "home":                         {url:"$A/modules/mdb/h/home.html",router:1},
-    }
-    for(m in modules){$vm.module_list[m]=modules[m];}
-})();
