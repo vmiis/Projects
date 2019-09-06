25f4f9b138268f876d7fc60c898100a456be2e7d
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-09-06 15:15:11 +1000

    update

--- sites/labs/013/module-list.js
+++ sites/labs/013/module-list.js
@@ -1,10 +1,10 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "p1":        {url:"$H/m/p1.html"},
-        "p2":        {url:"$H/m/p2.html"},
-        "p3":        {url:"$H/m/p3.html"},
-        "p4":        {url:"$H/m/p4.html"},
+        "p1":        {url:"$H/m/p1.html",Table:"demo-vm-ccontact-us"},
+        "p2":        {url:"$H/m/p2.html",Table:"demo-vm-ccontact-us"},
+        "p3":        {url:"$H/m/p3.html",Table:"demo-vm-ccontact-us"},
+        "admin":     {url:"$H/m/admin.html",Table:"demo-vm-ccontact-us",form_module:"p1"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
