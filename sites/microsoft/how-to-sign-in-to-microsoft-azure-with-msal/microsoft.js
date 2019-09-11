757bda3fede7148a43f57b76879207239a7956df
vmiis <zzm.vmiis@gmail.com>
2019-09-11 20:08:04 +1000

    update

--- sites/microsoft/how-to-sign-in-to-microsoft-azure-with-msal/microsoft.js
+++ sites/microsoft/how-to-sign-in-to-microsoft-azure-with-msal/microsoft.js
@@ -10,6 +10,8 @@ $vm.m365_init=function(){
     };
     $vm.msalConfig={
         auth: {
+            //clientId: '3bcb40c5-fec0-4b3b-ba67-f4d46d577f97', 
+            //redirectUri:h+"/microsoft-authentication-w.html",
             clientId: 'f39f8959-8cd7-4570-8c0f-548306bf899a', 
             redirectUri:h+"/microsoft-authentication.html",
         },
@@ -30,6 +32,7 @@ $vm.m365_init=function(){
         });
     }
     $vm.m365_signout=function() {
+	    $vm.clear_token();
         $vm.m365_msal.logout();
     }
     $vm.m365_graph=function(url,scope,callback){
@@ -38,7 +41,6 @@ $vm.m365_init=function(){
             var mt1=new Date().getTime();
             xmlHttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200){
-                    //$("#_sys_dev_info_elapsed").html( (this.response.length/1000).toFixed(1)+"kb/"+(new Date().getTime()-mt1).toString()+"ms");
                     var data=JSON.parse(this.responseText);
                     callback(data);
                 }
@@ -62,9 +64,22 @@ $vm.m365_init=function(){
     };
     //------------------------------------
     if($vm.m365_msal.getAccount()!=undefined){
-        console.log($vm.m365_msal.getAccount().userName);
+        var t1=(new Date().getTime() + 1) / 1000;
+        var t2=parseInt($vm.m365_msal.getAccount().idToken.exp);
+        console.log(t2);
+        console.log(t1);
+        var dt=t2-t1
+        console.log(dt);
+        if(dt<0){
+            alert("The session has expired.");
+            $vm.m365_signin();
+            return;
+        }
+
         $vm.m365_msal.acquireTokenSilent({scopes: ["user.read"]}).then(function (tokenResponse){
-            console.log("acquireTokenSilent succesfull.");
+            //console.log(tokenResponse);
+            //$vm.microsoft_token=tokenResponse.accessToken;
+            $vm.microsoft_token=tokenResponse.idToken.rawIdToken;
             $vm.user_name_3rd=$vm.m365_msal.getAccount().name;
             $vm.issuer_3rd="microsoft";
             $vm.user_name_365=$vm.m365_msal.getAccount().userName;
