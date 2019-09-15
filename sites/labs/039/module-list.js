3086cbd5f6ff01134b4dc2ae84a09fa13598cb03
vmiis <zzm.vmiis@gmail.com>
2019-09-15 20:28:49 +1000

    update

---
+++ sites/labs/039/module-list.js
@@ -0,0 +1,15 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var modules={
+        "pie-data": {url:"$H/m/pie-data.html",Table:"pie",form_module:"pie-form",router:1},
+        "pie-form": {url:"$H/m/pie-form.html",Table:"pie"},
+        "p1":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091501"},
+        "p2":       {url:"$H/m/week-calendar.html",Table:"demo-vm-19091501","booking":"p1"},
+        "p3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091501",form_module:"p1"},
+    }
+    for(p in modules){
+        $vm.module_list[p]=modules[p]; 
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    //-------------------------------------------------------------------------------------
+})();
