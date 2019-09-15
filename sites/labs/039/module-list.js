6725dc9b6c6c551af2c9cf7c049dbd90495b1140
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-09-16 06:45:30 +1000

    update

--- sites/labs/039/module-list.js
+++ sites/labs/039/module-list.js
@@ -3,9 +3,9 @@
     var modules={
         "pie-data": {url:"$H/m/pie-data.html",Table:"pie",form_module:"pie-form",router:1},
         "pie-form": {url:"$H/m/pie-form.html",Table:"pie"},
-        "p1":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091501"},
-        "p2":       {url:"$H/m/week-calendar.html",Table:"demo-vm-19091501","booking":"p1"},
-        "p3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091501",form_module:"p1"},
+        "p1":       {url:"$H/m/week-calendar.html",Table:"demo-vm-19091501","booking":"p2"},
+        "p2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091501"},
+        "p3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091501",form_module:"p2"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
