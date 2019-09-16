b7e449b3a54e163035b1a1be179117d98d912a16
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-09-16 16:24:03 +1000

    update

--- sites/labs/041/module-list.js
+++ sites/labs/041/module-list.js
@@ -1,9 +1,9 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "p1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091501","booking":"p2"},
-        "p2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091501"},
-        "p3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091501",form_module:"p2"},
+        "p1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091602","booking":"p2"},
+        "p2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091602"},
+        "p3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091602",form_module:"p2"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
