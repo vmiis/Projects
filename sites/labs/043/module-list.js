3657be316b6ff3afac5dc4cfd8495bf21181ab07
vmiis <zzm.vmiis@gmail.com>
2019-09-18 17:13:21 +1000

    update

--- sites/labs/043/module-list.js
+++ sites/labs/043/module-list.js
@@ -4,8 +4,8 @@
         "m1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091801","booking":"m2"},
         "m6":       {url:"$H/m/calendar-month.html",Table:"demo-vm-19091801","booking":"m2"},
         "m7":       {url:"$H/m/calendar-day.html",Table:"demo-vm-19091801","booking":"m2"},
-        "m2":       {url:"$H/m/roster-form.html",Table:"demo-vm-19091801"},
-        "m3":       {url:"$H/m/roster-data.html",Table:"demo-vm-19091801",form_module:"m2"},
+        "m2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091801"},
+        "m3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091801",form_module:"m2"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
--- sites/labs/044/module-list.js
+++ sites/labs/044/module-list.js
@@ -1,13 +1,13 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "m1":       {url:"$H/m/calendar-week.html", Table:"demo-vm-19091701","booking":"m2"},
-        "m6":       {url:"$H/m/calendar-month.html",Table:"demo-vm-19091701","booking":"m2"},
-        "m7":       {url:"$H/m/calendar-day.html",Table:"demo-vm-19091701","booking":"m2"},
-        "m2":       {url:"$H/m/roster-form.html",Table:"demo-vm-19091701",lookup:"demo-vm-19091702"},
-        "m3":       {url:"$H/m/roster-data.html",Table:"demo-vm-19091701",form_module:"m2"},
-        "m4":       {url:"$H/m/staff-data.html",Table:"demo-vm-19091702",form_module:"m5"},
-        "m5":       {url:"$H/m/staff-form.html",Table:"demo-vm-19091702"},
+        "m1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091802","booking":"m2"},
+        "m6":       {url:"$H/m/calendar-month.html",Table:"demo-vm-19091802","booking":"m2"},
+        "m7":       {url:"$H/m/calendar-day.html",Table:"demo-vm-19091802","booking":"m2"},
+        "m2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091802",lookup:"demo-vm-19091803"},
+        "m3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091802",form_module:"m2"},
+        "m4":       {url:"$H/m/participant-data.html",Table:"demo-vm-19091803",form_module:"m5"},
+        "m5":       {url:"$H/m/participant-form.html",Table:"demo-vm-19091803"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
