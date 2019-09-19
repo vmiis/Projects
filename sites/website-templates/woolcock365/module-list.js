1de3324aa84ecf5d3db46feecd93f9196fb70ede
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-09-19 16:08:22 +1000

    update

--- sites/website-templates/woolcock365/module-list.js
+++ sites/website-templates/woolcock365/module-list.js
@@ -243,6 +243,7 @@
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
         $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+        if($vm.module_list[p].Table!=undefined) $vm.module_list[p].Table=$vm.module_list[p].Table.replace("https://woolcockdev.crm6","https://woolcock.crm6");
     }
     //-------------------------------------------------------------------------------------
 })();
