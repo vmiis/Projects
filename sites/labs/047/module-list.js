d96859ceb1d5061bb8822af29dd6cfa7df29f318
vmiis <zzm.vmiis@gmail.com>
2019-09-21 18:05:45 +1000

    update

--- sites/labs/047/module-list.js
+++ sites/labs/047/module-list.js
@@ -1,10 +1,11 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "m1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091811","booking":"m2",lookup:"demo-vm-19091812"},
+        "m1":       {url:"$H/m/calendar-week.html",Table:"demo-vm-19091811","booking":"m2","booking2":"m8",lookup:"demo-vm-19091812"},
         "m6":       {url:"$H/m/calendar-month.html",Table:"demo-vm-19091811","booking":"m2",lookup:"demo-vm-19091812"},
         "m7":       {url:"$H/m/calendar-day.html",Table:"demo-vm-19091811","booking":"m2",lookup:"demo-vm-19091812"},
         "m2":       {url:"$H/m/booking-form.html",Table:"demo-vm-19091811",lookup:"demo-vm-19091812"},
+        "m8":       {url:"$H/m/booking-form.2.html",Table:"demo-vm-19091811",lookup:"demo-vm-19091812"},
         "m3":       {url:"$H/m/booking-data.html",Table:"demo-vm-19091811",form_module:"m2"},
         "m4":       {url:"$H/m/list-data.html",Table:"demo-vm-19091812",form_module:"m5"},
         "m5":       {url:"$H/m/list-form.html",Table:"demo-vm-19091812"},
