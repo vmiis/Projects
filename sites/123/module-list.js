add2b6e6b368a1579972c0fb185757597c2fa77e
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-10-11 15:55:26 +1100

    update

--- sites/123/module-list.js
+++ sites/123/module-list.js
@@ -1,8 +1,10 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "data":      {url:"$H/m/data.html",Table:"demo-vm-19100901",form_module:"form"},
-        "form":      {url:"$H/m/form.html",Table:"demo-vm-19100901"},
+        "url-data":      {url:"$H/m/url-data.html",Table:"demo-vm-19100901",form_module:"url-form"},
+        "url-form":      {url:"$H/m/url-form.html",Table:"demo-vm-19100901"},
+        "test-data":      {url:"$H/m/test-data.html",Table:"demo-vm-19100902",form_module:"test-form"},
+        "test-form":      {url:"$H/m/test-form.html",Table:"demo-vm-19100902"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
